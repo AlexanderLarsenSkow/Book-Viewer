587eb423b8043378bf900b3c3438c2c0607d386b
Alex <askow2015@gmail.com>
2025-02-04 15:18:46 -0700

    Added filter for @content instance var

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,21 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubi'
 
-def chapter_titles
-  File.readlines("data/toc.txt")
+before do
+  @contents = File.readlines("data/toc.txt")
 end
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapters/:number" do
   @number = params[:number].to_i 
-  @contents = File.readlines("data/toc.txt")
   @title = "Chapter #{@number}: #{@contents[@number - 1]}"
-
   @chapter = File.read("data/chp1.txt").split("\n\n")  
 
   erb :chapter
