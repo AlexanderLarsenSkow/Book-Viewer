2d3e10c9181205da615290de77ee5f701aca230d
Alex <askow2015@gmail.com>
2025-02-04 15:14:53 -0700

    Added Dynamic Route Parameters for Each Chapter

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,15 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubi'
 
+def chapter_titles
+  File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
-get "/chapters/1" do 
-  @title = "Chapter 1: A Scandal in Bohemia"
+get "/chapters/:number" do
+  @number = params[:number].to_i 
   @contents = File.readlines("data/toc.txt")
+  @title = "Chapter #{@number}: #{@contents[@number - 1]}"
+
   @chapter = File.read("data/chp1.txt").split("\n\n")  
 
   erb :chapter
