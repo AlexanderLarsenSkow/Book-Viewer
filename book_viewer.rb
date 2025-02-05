e959ff4c68ebe505d40c03da1b150214dd2b884e
Alex <askow2015@gmail.com>
2025-02-04 23:48:48 -0700

    Added view helper to organize text into paragraphsand correctly placed each chapter text on each page

--- book_viewer.rb
+++ book_viewer.rb
@@ -6,6 +6,12 @@ before do
   @contents = File.readlines("data/toc.txt")
 end
 
+helpers do
+  def in_paragraphs(chapter)
+    chapter.split("\n\n").map { |para| "<p>#{para}</p>"}.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   erb :home
@@ -14,7 +20,7 @@ end
 get "/chapters/:number" do
   @number = params[:number].to_i 
   @title = "Chapter #{@number}: #{@contents[@number - 1]}"
-  @chapter = File.read("data/chp1.txt").split("\n\n")  
+  @chapter = File.read("data/chp#{@number}.txt")  
 
   erb :chapter
 end
